affbefae3fcbf2a6f76f32e6cc2c189e3a719632
IRedFoxI <pimmesselink@gmail.com>
2013-04-11 16:48:07 +0100

    Forgot to re-add the files

---
+++ DateComparer.class.rb
@@ -0,0 +1,15 @@
+require 'date'
+require File.expand_path( File.dirname( __FILE__ ) + '/Loader.library.rb' )
+requireLibrary 'DataStructures'
+
+module MatchBot
+
+	class DateComparer < Kesh::DataStructures::Comparer
+	
+		def compare( a, b )
+			return ( a.date - b.date )
+		end
+		
+	end
+	
+end
